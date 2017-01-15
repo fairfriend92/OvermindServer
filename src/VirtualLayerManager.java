f1f7e83cc68588c4155ce6145305ef57676938e5
fairfriend92 <fairfriend92@gmail.com>
2017-01-15 21:13:39 +0000

    Modificata la classe LocalNetwork. Introdotta la lista di nodi disponibili e iniziata la sua popolazione.

--- src/VirtualLayerManager.java
+++ src/VirtualLayerManager.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
@@ -11,7 +12,7 @@ public class VirtualLayerManager extends Thread{
 	
 	public final static int SERVER_PORT = 4194;
 	
-	static boolean shutdown = false;
+	static boolean shutdown = false;	
 			
 	@Override
 	public void run() {
@@ -20,7 +21,11 @@ public class VirtualLayerManager extends Thread{
 		BlockingQueue<Socket> clientSocketsQueue = new ArrayBlockingQueue<>(16);
 		BlockingQueue<com.example.overmind.LocalNetwork> localNetworksQueue = new ArrayBlockingQueue<>(16);
 		Population population = new Population(); 
-		ExecutorService clientManagerExecutor = Executors.newCachedThreadPool();		
+		ExecutorService clientManagerExecutor = Executors.newCachedThreadPool();	
+		
+		ArrayList<com.example.overmind.LocalNetwork> availableNodes = new ArrayList<>();
+		ArrayList<com.example.overmind.LocalNetwork> unsyncNodes = new ArrayList<>();
+		
 		
 		// Create socket for this server
 		ServerSocket serverSocket = null;		
@@ -49,9 +54,79 @@ public class VirtualLayerManager extends Thread{
 			} catch (InterruptedException e) {
 				System.out.println(e);
 			}
-			
-			// Build the populations from the local networks
 			assert localNetwork != null;
+			
+			/**
+			 * Populate and update the list of terminals available for connection
+			 */
+			
+			// Iterate over all the available terminals
+			for (int i = 0; i < availableNodes.size() || 
+					(localNetwork.numOfDendrites == 0 && localNetwork.numOfSynapses == 0); i++) {
+				
+				com.example.overmind.LocalNetwork currentNode = availableNodes.get(i);
+				
+				// Branch depending on whether either the synapses or the dendrites of the current node are saturated
+				if (currentNode.numOfDendrites - localNetwork.numOfNeurons >= 0 &&
+						localNetwork.numOfSynapses - currentNode.numOfNeurons >= 0) {
+					
+					// Update the number of dendrites and synapses for the current node and the local network
+					currentNode.numOfDendrites -= localNetwork.numOfNeurons;
+					localNetwork.numOfSynapses -= currentNode.numOfNeurons;
+					
+					// Update the list of connected terminals for both the node and the local network
+					currentNode.presynapticNodes.add(localNetwork);
+					localNetwork.postsynapticNodes.add(currentNode);
+					
+					// Send to the list of terminals which need to be updated the current node
+					if (!unsyncNodes.contains(currentNode)) {
+						unsyncNodes.add(currentNode);
+					} else {
+						int index = unsyncNodes.indexOf(currentNode);
+						unsyncNodes.set(index, currentNode);
+					}
+					
+					// Update the current node in the list availableNodes
+					availableNodes.set(i, currentNode);
+					
+				} else if (currentNode.numOfSynapses - localNetwork.numOfNeurons >= 0 &&
+						localNetwork.numOfDendrites - currentNode.numOfNeurons >= 0) {
+					
+					/**
+					 * Just as before but now synapses and dendrites are exchanged
+					 */					
+					
+					currentNode.numOfSynapses -= localNetwork.numOfNeurons;
+					localNetwork.numOfDendrites -= currentNode.numOfNeurons;
+					
+					currentNode.postsynapticNodes.add(localNetwork);
+					localNetwork.presynapticNodes.add(currentNode);
+	
+					if (!unsyncNodes.contains(currentNode)) {
+						unsyncNodes.add(currentNode);
+					} else {
+						int index = unsyncNodes.indexOf(currentNode);
+						unsyncNodes.set(index, currentNode);
+					}
+					
+					availableNodes.set(i, currentNode);
+					
+				} else if (currentNode.numOfSynapses == 0 && currentNode.numOfDendrites == 0) { 
+					// If BOTH the synapses and the dendrites of the current node are saturated it can be removed
+					availableNodes.remove(i);
+				}
+				
+			}
+			
+			// If either the dendrites or the synapses of the local network are not saturated it can be added to the list
+			if (localNetwork.numOfDendrites > 0 || localNetwork.numOfSynapses > 0) {
+				availableNodes.add(localNetwork);
+			}	
+			
+			// Add the local network to the list of nodes that need to be sync with the physical terminals
+			unsyncNodes.add(localNetwork);
+			
+			// Build the populations from the local networks			
 			if (population.numOfNeurons + localNetwork.numOfNeurons < 1024) {
 				population.addrs.add(localNetwork.ip);
 				population.numOfNeurons += localNetwork.numOfNeurons;
--- src/com/example/overmind/LocalNetwork.java
+++ src/com/example/overmind/LocalNetwork.java
@@ -1,11 +1,19 @@
 package com.example.overmind;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
-public class LocalNetwork implements Serializable {		
-	public short numOfNeurons;
-	public String ip;
+public class LocalNetwork implements Serializable {
+    public short numOfNeurons, numOfDendrites, numOfSynapses;
+    public String ip;
+    public ArrayList<LocalNetwork> presynapticNodes;
+    public ArrayList<LocalNetwork> postsynapticNodes;   
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) { return true; }
+        if (obj == null || obj.getClass() != this.getClass()) { return false; }
+        LocalNetwork compare = (LocalNetwork) obj;
+        return compare.ip.equals(ip);
+    }
 }
-
-	
-	
