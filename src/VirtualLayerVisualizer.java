20c6fe6ba38a3fa29d94e017145418219d8bf703
fairfriend92 <fairfriend92@gmail.com>
2017-06-12 20:29:30 +0000

    Aggiunto paintAll

--- src/VirtualLayerVisualizer.java
+++ src/VirtualLayerVisualizer.java
@@ -16,6 +16,8 @@ import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 
 import javax.imageio.ImageIO;
@@ -35,15 +37,13 @@ import javax.swing.JToggleButton;
 public class VirtualLayerVisualizer extends Thread{
 	
 	static JFrame VLVFrame = new JFrame();
-	LayeredPaneVL layeredPaneVL = new LayeredPaneVL();
+	public LayeredPaneVL layeredPaneVL = new LayeredPaneVL();
 	
-	JToggleButton showTerminalFrame = new JToggleButton();
+	private JToggleButton showTerminalFrame = new JToggleButton();
 
-
-	boolean shutdown = false;
-	boolean resizeSelected = false;
-	Node selectedNode;
-	JLabel selectedNodeLabel;
+	private boolean shutdown = false;
+	private Node selectedNode;
+	private JLabel selectedNodeLabel;
 	private HashMap<Integer, JLabelVL> nodeIconsTable = new HashMap<>();
 	private JPanel infoPanel = new JPanel();
 
@@ -58,49 +58,60 @@ public class VirtualLayerVisualizer extends Thread{
 		JPanel totalPanel = new JPanel();
 		JPanel containerPanel = new JPanel();
 		JPanel commandsPanel = new JPanel();
+		JPanel colorsLegend = new JPanel();
 		
 		JButton resizeX = new JButton();
 		JButton resizeY = new JButton();
 		JToggleButton cutLink = new JToggleButton();
 		JToggleButton createLink = new JToggleButton();
+		JToggleButton paintAll = new JToggleButton();
 
-		
-		GridBagConstraints VLConstraint = new GridBagConstraints(), commandsConstraint = new GridBagConstraints();	
+			
+		/* Load icons for the buttons of the commands panel */
 		
 		try {
-			Image img = ImageIO.read(new FileInputStream("resources/icons/icons8-Cut.png"));
+			Image img = ImageIO.read(getClass().getResource("/icons/icons8-Cut.png"));
 			cutLink.setIcon(new ImageIcon(img.getScaledInstance(24, 24, Image.SCALE_SMOOTH)));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
 		try {
-			Image img = ImageIO.read(new FileInputStream("resources/icons/icons8-Pencil.png"));
+			Image img = ImageIO.read(getClass().getResource("/icons/icons8-Pencil.png"));
 			createLink.setIcon(new ImageIcon(img.getScaledInstance(24, 24, Image.SCALE_SMOOTH)));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
 		try {
-			Image img = ImageIO.read(new FileInputStream("resources/icons/icons8-Info.png"));
+			Image img = ImageIO.read(getClass().getResource("/icons/icons8-Info.png"));
 			showTerminalFrame.setIcon(new ImageIcon(img.getScaledInstance(24, 24, Image.SCALE_SMOOTH)));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}		
 		
 		try {
-			Image img = ImageIO.read(new FileInputStream("resources/icons/icons8-Width.png"));
+			Image img = ImageIO.read(getClass().getResource("/icons/icons8-Width.png"));
 			resizeX.setIcon(new ImageIcon(img.getScaledInstance(24, 24, Image.SCALE_SMOOTH)));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}		
 		
 		try {
-			Image img = ImageIO.read(new FileInputStream("resources/icons/icons8-Height.png"));
+			Image img = ImageIO.read(getClass().getResource("/icons/icons8-Height.png"));
 			resizeY.setIcon(new ImageIcon(img.getScaledInstance(24, 24, Image.SCALE_SMOOTH)));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}	
+		
+		try {
+			Image img = ImageIO.read(getClass().getResource("/icons/icons8-Paint Bucket.png"));
+			paintAll.setIcon(new ImageIcon(img.getScaledInstance(24, 24, Image.SCALE_SMOOTH)));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
+		
+		/* Action events for the buttons of the commands panel */
 		
 		showTerminalFrame.addItemListener(new ItemListener() {
 			public void itemStateChanged(ItemEvent ev) {
@@ -139,6 +150,17 @@ public class VirtualLayerVisualizer extends Thread{
 			}
 		});	
 		
+		paintAll.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent ev) {
+				if(ev.getStateChange()==ItemEvent.SELECTED)
+					layeredPaneVL.connPanel.paintAll = true;
+				else if(ev.getStateChange()==ItemEvent.DESELECTED)
+					layeredPaneVL.connPanel.paintAll = false;
+			}
+		});
+		
+		/* Commands panel */
+		
 		commandsPanel.setLayout(new GridLayout(3,2));
 		commandsPanel.setBorder(BorderFactory.createCompoundBorder(
 					BorderFactory.createTitledBorder("Commands"),
@@ -148,7 +170,9 @@ public class VirtualLayerVisualizer extends Thread{
 		commandsPanel.add(resizeX);
 		commandsPanel.add(resizeY);
 		commandsPanel.add(showTerminalFrame);
+		commandsPanel.add(paintAll);
 
+		/* Info panel */
 		
 		infoPanel.setLayout(new GridLayout(3,0));
 		infoPanel.setBorder(BorderFactory.createCompoundBorder(
@@ -156,11 +180,34 @@ public class VirtualLayerVisualizer extends Thread{
 					BorderFactory.createEmptyBorder(5,5,5,5)));
 		infoPanel.add(new JLabel("Select a node"));
 		
+		/* Colors legend panel */
+		
+		colorsLegend.setLayout(new GridLayout(2,0));
+		colorsLegend.setBorder(BorderFactory.createCompoundBorder(
+				BorderFactory.createTitledBorder("Colors legend"),
+				BorderFactory.createEmptyBorder(5,5,5,5)));
+		JLabel preConn = new JLabel("Presynaptic conn.");
+		preConn.setForeground(Color.red);
+		JLabel postConn = new JLabel("Postsynaptic conn.");
+		postConn.setForeground(Color.blue);
+		colorsLegend.add(preConn);
+		colorsLegend.add(postConn);
+		
+		/* Container panel */
+		
+		// This panel contains the commands, info and colors legend panels
+		
 		containerPanel.setLayout(new BoxLayout(containerPanel, BoxLayout.Y_AXIS));		
 		containerPanel.add(commandsPanel);
 		containerPanel.add(Box.createRigidArea(new Dimension(0,5)));		
 		containerPanel.add(infoPanel);
+		containerPanel.add(Box.createRigidArea(new Dimension(0,5)));		
+		containerPanel.add(colorsLegend);
 				
+		// Constraints used for the total panel layout
+		
+		GridBagConstraints VLConstraint = new GridBagConstraints(), commandsConstraint = new GridBagConstraints();	
+		
 		VLConstraint.gridx = 0;
 		VLConstraint.gridy = 0;
 		VLConstraint.gridwidth = 2;	
@@ -172,6 +219,10 @@ public class VirtualLayerVisualizer extends Thread{
 		commandsConstraint.gridy = 0;
 		commandsConstraint.fill = GridBagConstraints.HORIZONTAL;
 					
+		/* Total panel */
+		
+		// Total panel is made of the container panel and the main view of the VLV, scroll pane
+				
 		totalPanel.setLayout(new GridBagLayout());
 		totalPanel.add(scrollPane, VLConstraint);
 		totalPanel.add(containerPanel, commandsConstraint);
@@ -185,8 +236,12 @@ public class VirtualLayerVisualizer extends Thread{
 				
 	}
 	
+	/* Class for the panel which acts as a canvas on which we paint the synaptic connections */
+	
 	public class DrawablePanelVL extends JPanel {		
 
+		public boolean paintAll = false;
+		
 		public DrawablePanelVL () {
 			this.setPreferredSize(new Dimension(640, 480));
 			this.setOpaque(true);
@@ -194,28 +249,109 @@ public class VirtualLayerVisualizer extends Thread{
 			this.setBorder(BorderFactory.createLineBorder(Color.black));
 		}
 		
+		// Extends the paint method to draw the synaptic connections in addition to everything else		
 		public void paintComponent(Graphics g) {
 	        super.paintComponent(g); 
 
 	        Point tmpPoint;
 	        int radius;
 	        
-	        if (selectedNode != null) {
-	        	for (int i = 0; i < selectedNode.presynapticNodes.size(); i++) {
-	        		if (selectedNode.presynapticNodes.get(i).terminal.ip != VirtualLayerManager.serverIP) {
-	        			tmpPoint = nodeIconsTable.get(selectedNode.presynapticNodes.get(i).ipHashCode).nodeLabel.getLocation();
-	        			radius = nodeIconsTable.get(selectedNode.presynapticNodes.get(i).ipHashCode).nodeLabel.getWidth() / 2;
-	        			g.drawLine(selectedNodeLabel.getX() + selectedNodeLabel.getWidth() / 2, selectedNodeLabel.getY() + selectedNodeLabel.getHeight() / 2, 
-	        					tmpPoint.x + radius, tmpPoint.y + radius);
-	        		}
-	        	}
-	        }
+	        JLabelVL[] nodeIconsArray = new JLabelVL[nodeIconsTable.size()];
+	        int forIterations = 1;
+	        
+	        Node node;
+	        JLabel nodeLabel;
+	        
+	        if (paintAll) {
+	        	nodeIconsArray = nodeIconsTable.values().toArray(new JLabelVL[nodeIconsTable.size()]);	        	
+	        	forIterations = nodeIconsArray.length;
+	        } 
+	        
+	        for (int j = 0; j < forIterations; j++) {
+	        	
+	        	node = paintAll ? nodeIconsArray[j].node : selectedNode;
+	        	nodeLabel = paintAll ? nodeIconsArray[j].nodeLabel : selectedNodeLabel;
+	        		        	
+				// Draw the connections only for the selected node
+				if (node != null) {
+
+					g.setColor(Color.black);
+					
+					// Write node IP on top of the selected node icon
+					g.drawString(node.terminal.ip, nodeLabel.getX(), nodeLabel.getY());
+
+					// Presynaptic connections are drawn in red
+					g.setColor(Color.red);
+
+					// Iterate over the presynaptic connections of the node selected
+					for (int i = 0; i < node.presynapticNodes.size(); i++) {
+
+						// For the i-th connections, proceeds only if it doesn't belong to the server itself
+						if (node.presynapticNodes.get(i).terminal.ip != VirtualLayerManager.serverIP) {
+
+							// Store the location of the icon representing the i-th node
+							tmpPoint = nodeIconsTable.get(node.presynapticNodes.get(i).ipHashCode).nodeLabel
+									.getLocation();
+
+							// Retrieve the radius of the icon of the i-th node
+							radius = nodeIconsTable.get(node.presynapticNodes.get(i).ipHashCode).nodeLabel
+									.getWidth() / 2;
+
+							// Draw the line 
+							g.drawLine(nodeLabel.getX() + nodeLabel.getWidth() / 2,
+									nodeLabel.getY() + nodeLabel.getHeight() / 2, tmpPoint.x + radius,
+									tmpPoint.y + radius);
+
+							// Write the number of connections that the line represents just under it
+							g.drawString(String.valueOf(node.terminal.numOfNeurons),
+									(nodeLabel.getX() + tmpPoint.x) / 2,
+									(nodeLabel.getY() + tmpPoint.y) / 2);
+
+						}
+					}
+
+					// Postsynaptic connections
+					g.setColor(Color.blue);
+
+					// Just like the previous for, but this time with post instead of pre connections
+					for (int i = 0; i < node.postsynapticNodes.size(); i++) {
+
+						if (node.postsynapticNodes.get(i).terminal.ip != VirtualLayerManager.serverIP) {
+
+							tmpPoint = nodeIconsTable.get(node.postsynapticNodes.get(i).ipHashCode).nodeLabel
+									.getLocation();
+
+							radius = nodeIconsTable.get(node.postsynapticNodes.get(i).ipHashCode).nodeLabel
+									.getWidth() / 2;
+
+							g.drawLine(nodeLabel.getX() + nodeLabel.getWidth() / 2,
+									nodeLabel.getY() + nodeLabel.getHeight() / 2 + 4, tmpPoint.x + radius,
+									tmpPoint.y + radius + 4);
+
+							g.drawString(String.valueOf(node.postsynapticNodes.get(i).terminal.numOfNeurons),
+									(nodeLabel.getX() + tmpPoint.x) / 2,
+									(nodeLabel.getY() + tmpPoint.y) / 2 + 36);
+						}
+						/* [End of inner if] */
+
+					}
+					/* [End of inner for] */
+
+				}
+				/* [End of outer if] */
+				
+			}
+	        /* [End of outer for] */
 	        
 		}
+		/* [End of paint method] */
 		
 	}
+	/* [End of DrawablePanelVL class] */
 		
 	// TODO Resizing of the window should be achieved by implementing Scrollable
+	
+	
 	
 	public class LayeredPaneVL extends JPanel implements MouseMotionListener {
 		
@@ -242,6 +378,8 @@ public class VirtualLayerVisualizer extends Thread{
 			JLabelVL newNodeLabel = new JLabelVL(node);
 			nodeIconsTable.put(node.ipHashCode, newNodeLabel);
 			VLPanel.add(newNodeLabel.nodeLabel, new Integer(1), nodeIconsTable.size());
+			connPanel.revalidate();
+			connPanel.repaint();
 						
 		}
 		
@@ -295,7 +433,7 @@ public class VirtualLayerVisualizer extends Thread{
 			this.dimension = (int) (48.0f - (24.0f / 1023.0f) * (1024.0f - (float)n.terminal.numOfNeurons));
 					
 			try {
-				Image img = ImageIO.read(new FileInputStream("resources/icons/icons8-New Moon.png"));
+				Image img = ImageIO.read(getClass().getResource("/icons/icons8-New Moon.png"));
 				nodeIcon = (new ImageIcon(img.getScaledInstance(this.dimension, this.dimension, Image.SCALE_SMOOTH)));
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -332,7 +470,7 @@ public class VirtualLayerVisualizer extends Thread{
 				infoPanel.repaint();
 				
 				try {
-					Image img = ImageIO.read(new FileInputStream("resources/icons/icons8-0 Percents.png"));
+					Image img = ImageIO.read(getClass().getResource("/icons/icons8-0 Percents.png"));
 					nodeIcon = (new ImageIcon(img.getScaledInstance(this.dimension, this.dimension, Image.SCALE_SMOOTH)));
 				} catch (IOException e1) {
 					e1.printStackTrace();
@@ -344,6 +482,9 @@ public class VirtualLayerVisualizer extends Thread{
 				mousePressed = true;
 				selectedNode = this.node;
 				selectedNodeLabel = nodeIconsTable.get(selectedNode.ipHashCode).nodeLabel;
+				
+				layeredPaneVL.connPanel.revalidate();
+				layeredPaneVL.connPanel.repaint();
 				
 			} else if (selectedNode == this.node) {
 				
@@ -357,7 +498,7 @@ public class VirtualLayerVisualizer extends Thread{
 				infoPanel.repaint();
 				
 				try {
-					Image img = ImageIO.read(new FileInputStream("resources/icons/icons8-New Moon.png"));
+					Image img = ImageIO.read(getClass().getResource("/icons/icons8-New Moon.png"));
 					nodeIcon = (new ImageIcon(img.getScaledInstance(this.dimension, this.dimension, Image.SCALE_SMOOTH)));
 				} catch (IOException e1) {
 					e1.printStackTrace();
@@ -367,6 +508,10 @@ public class VirtualLayerVisualizer extends Thread{
 				
 				nodeLabel.setIcon(nodeIcon);
 				mousePressed = false;	
+				
+				layeredPaneVL.connPanel.revalidate();
+				layeredPaneVL.connPanel.repaint();
+				
 			}
 			
 		}
